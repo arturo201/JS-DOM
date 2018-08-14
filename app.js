29a5590afcc27d009dd42bc9ec312eb9f2e1a74a
Arturo <arturoeslava88@gmail.com>
2018-08-14 15:00:57 +0000

    lesson 13

--- app.js
+++ app.js
@@ -26,12 +26,17 @@ deleteBtn.textContent = 'delete';
 bookName.textContent = 'value';
 
 
+// add classes
+bookName.classList.add('name');
+deleteBtn.classList.add('delete');
+
+
 // append to document 
 li.appendChild(bookName);
 li.appendChild(deleteBtn);
 list.appendChild(li);
   
-  
+
   
 });
 
